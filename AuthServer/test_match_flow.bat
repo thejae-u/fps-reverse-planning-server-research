520f58230fdf1d8c8fefa34646b57d1a6e7d172b
thejae-u <31125543+thejae-u@users.noreply.github.com>
2026-09-23 02:43:02 +0900

    del: old file delete

--- AuthServer/test_match_flow.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-chcp 65001 > nul
-title "AuthServer & Dedicated Server Match Test"
-
-powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0test_match_flow.ps1"
-
-echo.
-echo Press any key to exit.
-pause > nul
